81013e992e8df81a902d10d0872f57548e95ed62
mprcodes <mauriciopaternina@icloud.com>
2023-02-27 17:32:08 -0500

    revisit lesson 3

--- 03-Basic-Scene/script.js
+++ 03-Basic-Scene/script.js
@@ -1,29 +1,41 @@
 // HTML Element where result will be rendered to
 const canvas = document.getElementById('webgl')
-const sizes = {
-  width: 800,
-  height: 600,
-}
-
-// Creating a scene
 const scene = new THREE.Scene()
 
 // Creating a camera
+const cameraSizes = {
+  width: 800,
+  height: 600
+}
+const cameraFrustumAspectRatio = cameraSizes.width / cameraSizes.height
+const cameraFrustumVerticalFieldOfView = 64
+const cameraFrustumNearPlane = 0.1
+const cameraFrustumFarPlane = 1000
+
 /**
  * Creates a PerspectiveCamera.
  * @param {number} FOV - The field of view: the extent of the scene that is seen on the display at any given moment. The value is in degrees.
- * @param {number} aspectRatio - The size of the view: sizes.width / sizes.height.
+ * @param {number} aspectRatio - The size of the view: cameraSizes.width / cameraSizes.height.
  * @param {number} near - The near clipping plane.
  * @param {number} far - The far clipping plane.
  */
-// function THREE.PerspectiveCamera(fieldOfView, aspectRatio, near, far) {}
-
 const camera = new THREE.PerspectiveCamera(
-  64,
-  sizes.width / sizes.height,
-  0.1,
-  1000,
+  cameraFrustumVerticalFieldOfView,
+  cameraFrustumAspectRatio,
+  cameraFrustumNearPlane,
+  cameraFrustumFarPlane
 )
+
+const geometry = new THREE.BoxGeometry(1, 1, 1)
+// const material = new THREE.MeshBasicMaterial({ color: 0x66ff22 })
+// Changing material type so that light gets reflected on the box
+const material = new THREE.MeshPhongMaterial({ color: 0x66ff22 })
+
+// Create the cube from the geometry and the material defined above
+const cube = new THREE.Mesh(geometry, material)
+
+// Add the cube to the scene
+scene.add(cube)
 
 // Creating the renderer
 /*
@@ -31,30 +43,18 @@ const camera = new THREE.PerspectiveCamera(
   Threejs draws the result in a canvas via WebGL
 */
 const renderer = new THREE.WebGLRenderer({
-  canvas,
+  canvas
 })
 
 // Settting the renderer's size
-renderer.setSize(sizes.width, sizes.height)
-
-// Creating the geometry
-const geometry = new THREE.BoxGeometry(1, 1, 1)
-
-// Creating the material
-// const material = new THREE.MeshBasicMaterial({ color: 0x66ff22 })
-// Changin material type so that light gets reflected on the box
-const material = new THREE.MeshPhongMaterial({ color: 0x66ff22 })
-
-// Create the cube with the geometry and the material
-const cube = new THREE.Mesh(geometry, material)
-
-// Add the cube to the scene
-scene.add(cube)
+renderer.setSize(cameraSizes.width, cameraSizes.height)
 
 // Adding a light for better 3D perception
 {
   const directionalLight = new THREE.DirectionalLight(0xffffff, 0.72)
+
   directionalLight.position.set(-1, 2, 4)
+
   scene.add(directionalLight)
 }
 
